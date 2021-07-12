3be62846bd81f89dedf335b148a30c7c0bf3f616
kanchangawand <57029855+kanchangawand@users.noreply.github.com>
2021-07-12 16:56:44 +0530

    router

--- app/routers/router.js
+++ app/routers/router.js
@@ -17,6 +17,10 @@ router.get('/api/products/getAllProducts', products.findAll);
 router.put('/api/products/updateProduct/:id', products.update);
 router.delete('/api/products/deleteProduct/:id', products.delete);
 router.get('/api/products/pagination', products.pagingOfProduct);
+router.get('/api/products/findbyId/:id', products.findbyId);
+router.delete('/api/products/deleteProduct', products.deleteAll);
+
+//localhost:6001/api/products/pagination?page=0&size=6
 
 
 module.exports = router;
