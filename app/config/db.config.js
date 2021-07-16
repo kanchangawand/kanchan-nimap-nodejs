48293d0228735111f1d8d4dd922c07063b735ed2
kanchangawand <57029855+kanchangawand@users.noreply.github.com>
2021-07-16 19:32:31 +0530

    One to many relationship

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -12,5 +12,6 @@ db.sequelize = sequelize;
 
 db.Category = require('../models/category.model.js')(sequelize, Sequelize);
 db.Product = require('../models/product.model.js')(sequelize, Sequelize);
- 
+
+db.Product.belongsTo(db.Category);
 module.exports = db;
